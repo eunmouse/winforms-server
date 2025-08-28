c307ffa5f8b396b4747e47f48e9d6f491a8bd876
eunmouse <ejha1208@gmail.com>
2025-08-28 15:47:00 +0900

    서버 연결

---
+++ MyServer_001/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace MyServer_001
+{
+    partial class frmServer
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblIP = new System.Windows.Forms.Label();
+            this.lblPort = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.rtbChat = new System.Windows.Forms.RichTextBox();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblIP
+            // 
+            this.lblIP.AutoSize = true;
+            this.lblIP.Location = new System.Drawing.Point(12, 9);
+            this.lblIP.Name = "lblIP";
+            this.lblIP.Size = new System.Drawing.Size(44, 12);
+            this.lblIP.TabIndex = 0;
+            this.lblIP.Text = "IP 주소";
+            // 
+            // lblPort
+            // 
+            this.lblPort.AutoSize = true;
+            this.lblPort.Location = new System.Drawing.Point(132, 9);
+            this.lblPort.Name = "lblPort";
+            this.lblPort.Size = new System.Drawing.Size(53, 12);
+            this.lblPort.TabIndex = 1;
+            this.lblPort.Text = "포트번호";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(109, 17);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 12);
+            this.label3.TabIndex = 2;
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(9, 24);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(119, 21);
+            this.txtIP.TabIndex = 3;
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(134, 24);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(65, 21);
+            this.txtPort.TabIndex = 4;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(226, 7);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(91, 38);
+            this.btnConnect.TabIndex = 5;
+            this.btnConnect.Text = "연결";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // rtbChat
+            // 
+            this.rtbChat.Location = new System.Drawing.Point(9, 51);
+            this.rtbChat.Name = "rtbChat";
+            this.rtbChat.Size = new System.Drawing.Size(308, 107);
+            this.rtbChat.TabIndex = 6;
+            this.rtbChat.Text = "";
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(9, 164);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(227, 21);
+            this.txtMessage.TabIndex = 7;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(242, 164);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 8;
+            this.btnSend.Text = "전송";
+            this.btnSend.UseVisualStyleBackColor = true;
+            // 
+            // frmServer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(327, 197);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.rtbChat);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.txtIP);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblPort);
+            this.Controls.Add(this.lblIP);
+            this.Name = "frmServer";
+            this.Text = "Server";
+            this.Load += new System.EventHandler(this.frmServer_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblIP;
+        private System.Windows.Forms.Label lblPort;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.RichTextBox rtbChat;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Button btnSend;
+    }
+}
+
---
+++ MyServer_001/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace MyServer_001
+{
+    public partial class frmServer : Form
+    {
+        private TcpListener server;
+        private TcpClient client;
+
+        public frmServer()
+        {
+            InitializeComponent();
+        }
+
+        private void Listen()
+        {
+            // 서버 연결
+            try
+            {
+                int portNum = Convert.ToInt32(txtPort.Text);
+                server = new TcpListener(IPAddress.Parse(txtIP.Text), portNum);
+                server.Start();
+                writeRtbChat("서버 준비... 클라이언트 기다리는 중...");
+
+                // 클라이언트의 연결 요청이 오면 TcpClient 반환
+                client = server.AcceptTcpClient();
+                writeRtbChat("클라이언트 연결됨...");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"***연결 실패*** {ex.Message}");
+            }
+        }
+        
+        private void writeRtbChat(string str)
+        {
+            // rtbChat 객체는 UI 스레드(메인 스레드) 에서만 접근 가능하여, Invoke 로 넘겨서 처리 
+            if (rtbChat.InvokeRequired == true)
+            {
+                rtbChat.Invoke((MethodInvoker)(() =>
+                {
+                    rtbChat.Text = str + Environment.NewLine; // 줄바꿈
+                }));
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // 메인 스레드와 독립적으로 실행되는 작업 스레드 생성
+            Thread listenThread = new Thread(new ThreadStart(Listen));
+            listenThread.IsBackground = true;
+            listenThread.Start();
+        }
+
+        private void frmServer_Load(object sender, EventArgs e)
+        {
+            txtIP.Text = "127.1.1.0";
+            txtPort.Text = "5003";
+        }
+    }
+}
